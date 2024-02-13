89566dd00c1ac38d055cc37ba0deed08aceb4076
Requiem493 <akshaj.illa@gmail.com>
2024-02-13 15:32:58 -0500

    Added a function for addition and subtraction

--- Calc.java
+++ Calc.java
@@ -6,6 +6,7 @@ public class Calc {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         
+        int d = 0;
         
         System.out.println("Enter a number:");
         int x = scan.nextInt();
@@ -13,7 +14,22 @@ public class Calc {
         System.out.println("Enter a second number:");
         int y = scan.nextInt();
 
-        System.out.println(x + y);
+        System.out.println("Would you like to add or subtract the numbers?");
+        System.out.println("Type 1 for add and 2 for subtract");
+        d = scan.nextInt();
+
+        while(d != 1 || d != 2){
+        System.out.println("Would you like to add or subtract the numbers?");
+        System.out.println("Type 1 for add and 2 for subtract");
+        d = scan.nextInt();
+        }
+
+        if (d == 1){
+            System.out.println(x+y);
+        }else if(d == 2){
+            System.out.println(x-y);
+        }
+        
 
         scan.close();
 
