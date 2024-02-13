1ac0e5577fec900235df5324716223f288f260f6
Requiem493 <akshaj.illa@gmail.com>
2024-02-13 15:21:13 -0500

    Added Addition function to main()

--- Calc.java
+++ Calc.java
@@ -1,9 +1,22 @@
 package Calculator;
 
-public class Calc {
+import java.util.Scanner;
 
+public class Calc {
     public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
         
+        
+        System.out.println("Enter a number:");
+        int x = scan.nextInt();
+
+        System.out.println("Enter a second number:");
+        int y = scan.nextInt();
+
+        System.out.println(x + y);
+
+        scan.close();
+
     }
     
 }
